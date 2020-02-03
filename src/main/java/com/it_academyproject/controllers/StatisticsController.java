b42aae1c32e2713ba40f9cdf1f2bd48ff347bade
Ali Elias <alitunja27@gmail.com>
2020-02-03 16:40:26 +0100

    Adding Gson dependency to create Json objects

--- src/main/java/com/it_academyproject/controllers/StatisticsController.java
+++ src/main/java/com/it_academyproject/controllers/StatisticsController.java
@@ -1,14 +1,11 @@
-
 package com.it_academyproject.controllers;
 
-import com.fasterxml.jackson.core.JsonEncoding;
 import com.it_academyproject.services.StatisticsService;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
