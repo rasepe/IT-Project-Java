8133d75d9b41e4fe1b34069e3c77c007bb80f663
Rafel <rasepe@gmail.com>
2020-03-03 23:30:18 +0100

    commit B-9 03/03/20

--- src/main/java/com/it_academyproject/controllers/UserExerciseController.java
+++ src/main/java/com/it_academyproject/controllers/UserExerciseController.java
@@ -1,63 +1,129 @@
 
 package com.it_academyproject.controllers;
 
+
 import com.fasterxml.jackson.annotation.JsonView;
+import com.it_academyproject.domains.Exercice;
+import com.it_academyproject.domains.MyAppUser;
+import com.it_academyproject.domains.UserExercice;
+import com.it_academyproject.repositories.UserExerciceRepository;
 import com.it_academyproject.services.UserExerciseService;
 import com.it_academyproject.tools.View;
+
+
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserExerciseController
 {
-    @Autowired
-    UserExerciseService userExerciseService;
+	@Autowired
+	UserExerciseService userExerciseService;
+
+	@Autowired
+	UserExerciceRepository userExerciceRepository;
+
+	@GetMapping ("/api/userExercise/{itineraryId}")
+	public ResponseEntity<String>  getExerciseStudentByItinerary (@PathVariable String itineraryId )
+	{
+		try
+		{
+			JSONObject sendData = userExerciseService.getExerciseStudentByItinerary( itineraryId );
+			return new ResponseEntity( sendData.toString() , HttpStatus.FOUND);
+		}
+		catch (Exception e)
+		{
+			String exceptionMessage = e.getMessage();
+			JSONObject sendData = new JSONObject();
+			JSONObject message = new JSONObject();
+			message.put("type" , "error");
+			message.put("message" , exceptionMessage);
+			sendData.put("Message" , message);
+			return new ResponseEntity( sendData.toString() , HttpStatus.BAD_REQUEST);
+		}
+	}
+	@GetMapping ("/api/userExercise")
+	public ResponseEntity  getExerciseStudentByItinerary ( )
+	{
+		try
+		{
+			//get a list of all the itnerarys
+
+			JSONObject sendData = userExerciseService.getExerciseStudentByItinerary( );
+			return new ResponseEntity( sendData.toString() , HttpStatus.FOUND);
+		}
+		catch (Exception e)
+		{
+			String exceptionMessage = e.getMessage();
+			JSONObject sendData = new JSONObject();
+			JSONObject message = new JSONObject();
+			message.put("type" , "error");
+			message.put("message" , exceptionMessage);
+			sendData.put("Message" , message);
+			return new ResponseEntity( sendData.toString() , HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@JsonView(View.Summary.class)
+	@GetMapping ("/api/userExercise/Student_id")
+	public ResponseEntity<String>  getExercicesbyStudentId (@RequestBody MyAppUser student){
+
+		try
+		{
+			JSONObject sendData = userExerciseService.getExerciseStudentByStudent(student);
+			return new ResponseEntity( sendData.toString() , HttpStatus.FOUND);
+		}
+		catch (Exception e)
+		{
+			String exceptionMessage = e.getMessage();
+			JSONObject sendData = new JSONObject();
+			JSONObject message = new JSONObject();
+			message.put("type" , "error");
+			message.put("message" , exceptionMessage);
+			sendData.put("Message" , message);
+			return new ResponseEntity( sendData.toString() , HttpStatus.BAD_REQUEST);
+		}
+	}
 
 
-    @GetMapping ("/api/userExercise/{itineraryId}")
-    public ResponseEntity<String>  getExerciseStudentByItinerary (@PathVariable String itineraryId )
-    {
-        try
-        {
-            JSONObject sendData = userExerciseService.getExerciseStudentByItinerary( itineraryId );
-            return new ResponseEntity( sendData.toString() , HttpStatus.FOUND);
-        }
-        catch (Exception e)
-        {
-            String exceptionMessage = e.getMessage();
-            JSONObject sendData = new JSONObject();
-            JSONObject message = new JSONObject();
-            message.put("type" , "error");
-            message.put("message" , exceptionMessage);
-            sendData.put("Message" , message);
-            return new ResponseEntity( sendData.toString() , HttpStatus.BAD_REQUEST);
-        }
-    }
-    @GetMapping ("/api/userExercise")
-    public ResponseEntity  getExerciseStudentByItinerary ( )
-    {
-        try
-        {
-            //get a list of all the itnerarys
 
-            JSONObject sendData = userExerciseService.getExerciseStudentByItinerary( );
-            return new ResponseEntity( sendData.toString() , HttpStatus.FOUND);
-        }
-        catch (Exception e)
-        {
-            String exceptionMessage = e.getMessage();
-            JSONObject sendData = new JSONObject();
-            JSONObject message = new JSONObject();
-            message.put("type" , "error");
-            message.put("message" , exceptionMessage);
-            sendData.put("Message" , message);
-            return new ResponseEntity( sendData.toString() , HttpStatus.BAD_REQUEST);
-        }
-    }
+
+	//SET @CrossOrigin BEFORE DEPLOYING TO PRODUCTION!
+	@CrossOrigin
+	@JsonView(View.Summary.class)
+	@PutMapping("/api/userExercise/exercice_id")
+	public boolean setUserExerciseStatusData(@RequestBody UserExercice userExercice) { 
+
+
+		//UserExercice change;
+		List <UserExercice> list = userExerciceRepository.findAll();
+		for (int i=0; i<list.size(); i++) {
+			if (list.get(i).getId() == userExercice.getId()) {
+				Date date = new Date();	
+				list.get(i).setDate_status(date);
+				list.get(i).setStatusExercice(userExercice.getStatusExercice());
+				return true;
+			}
+		}
+
+		return false;
+
+
+	}
+
+
+
 }
 
--- src/main/java/com/it_academyproject/services/UserExerciseService.java
+++ src/main/java/com/it_academyproject/services/UserExerciseService.java
@@ -18,70 +18,139 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class UserExerciseService
 {
-    @Autowired
-    UserExerciceRepository userExerciceRepository;
-    @Autowired
-    MyAppUserRepository myAppUserRepository;
-    @Autowired
-    CourseRepository courseRepository;
-    @Autowired
-    ItineraryRepository itineraryRepository;
+	@Autowired
+	UserExerciceRepository userExerciceRepository;
+	@Autowired
+	MyAppUserRepository myAppUserRepository;
+	@Autowired
+	CourseRepository courseRepository;
+	@Autowired
+	ItineraryRepository itineraryRepository;
 
-    public JSONObject getExerciseStudentByItinerary (String itineraryIdString )
-    {
-        try
-        {
-            //convert the itineraryIdString to a int
-            int itineraryId = Integer.parseInt(itineraryIdString);
-            Itinerary itinerary = itineraryRepository.findOneById( itineraryId );
-            //get only active students
-            List<Course> courseList = courseRepository.findByItineraryAndEndDate(itinerary , null);
-            List<MyAppUser> myAppUserList = new ArrayList<>();
-            Map<String , List<UserExercice>> userUserExerciceMap = new HashMap<>();
-            for (int i = 0; i < courseList.size() ; i++)
-            {
-                MyAppUser myAppUser = courseList.get( i ).getUserStudent();
-                myAppUserList.add( myAppUser);
-                List<UserExercice> userExerciceList = userExerciceRepository.findByUserStudent(myAppUser);
-                //remove the Student field that is not necessary
-                for (int j = 0; j < userExerciceList.size(); j++)
-                {
-                    userExerciceList.get(j).setUserStudent ( null );
-                }
-                userUserExerciceMap.put(myAppUser.getId() , userExerciceList );
-            }
-            JSONObject sendData = new JSONObject( userUserExerciceMap );
-            return sendData;
-        }
-        catch (Exception e)
-        {
-            String exceptionMessage = e.getMessage();
-            JSONObject sendData = new JSONObject();
-            JSONObject message = new JSONObject();
-            message.put("type" , "error");
-            message.put("message" , exceptionMessage);
-            sendData.put("Message" , message);
-            return sendData;
-        }
-    }
-    public JSONObject getExerciseStudentByItinerary (  )
-    {
-        //get all the itineraries
-        List<Itinerary> itineraryList = itineraryRepository.findAll();
-        JSONObject sendData = new JSONObject();
-        for (int i = 0; i < itineraryList.size(); i++)
-        {
-            String itineraryId = Integer.toString( itineraryList.get(i).getId() );
-            JSONObject returnJson = getExerciseStudentByItinerary ( itineraryId );
-            sendData.put("Itinerary_" + itineraryId , returnJson );
+	public JSONObject getExerciseStudentByItinerary (String itineraryIdString )
+	{
+		try
+		{
+			//convert the itineraryIdString to a int
+			int itineraryId = Integer.parseInt(itineraryIdString);
+			Itinerary itinerary = itineraryRepository.findOneById( itineraryId );
+			//get only active students
+			List<Course> courseList = courseRepository.findByItineraryAndEndDate(itinerary , null);
+			List<MyAppUser> myAppUserList = new ArrayList<>();
+			Map<String , List<UserExercice>> userUserExerciceMap = new HashMap<>();
+			for (int i = 0; i < courseList.size() ; i++)
+			{
+				MyAppUser myAppUser = courseList.get( i ).getUserStudent();
+				myAppUserList.add( myAppUser);
+				List<UserExercice> userExerciceList = userExerciceRepository.findByUserStudent(myAppUser);
+				//remove the Student field that is not necessary
+				for (int j = 0; j < userExerciceList.size(); j++)
+				{
+					userExerciceList.get(j).setUserStudent ( null );
+				}
+				userUserExerciceMap.put(myAppUser.getId() , userExerciceList );
+			}
+			JSONObject sendData = new JSONObject( userUserExerciceMap );
+			return sendData;
+		}
+		catch (Exception e)
+		{
+			String exceptionMessage = e.getMessage();
+			JSONObject sendData = new JSONObject();
+			JSONObject message = new JSONObject();
+			message.put("type" , "error");
+			message.put("message" , exceptionMessage);
+			sendData.put("Message" , message);
+			return sendData;
+		}
+	}
+
+	public JSONObject getExerciseStudentByStudent(MyAppUser student) {
+		try
+		{
+
+			//get only active students
+			List<Course> courseList = courseRepository.findByUserStudent(student);
+			List<MyAppUser> myAppUserList = new ArrayList<>();
+			Map<String , List<UserExercice>> userUserExerciceMap = new HashMap<>();
+			for (int i = 0; i < courseList.size() ; i++)
+			{
+				MyAppUser myAppUser = courseList.get( i ).getUserStudent();
+				myAppUserList.add( myAppUser);
+				List<UserExercice> userExerciceList = userExerciceRepository.findByUserStudent(myAppUser);
+				//remove the Student field that is not necessary
+				for (int j = 0; j < userExerciceList.size(); j++)
+				{
+					userExerciceList.get(j).setUserStudent ( null );
+				}
+				userUserExerciceMap.put(myAppUser.getId() , userExerciceList );
+			}
+			JSONObject sendData = new JSONObject( userUserExerciceMap );
+			return sendData;
+		}
+		catch (Exception e)
+		{
+			String exceptionMessage = e.getMessage();
+			JSONObject sendData = new JSONObject();
+			JSONObject message = new JSONObject();
+			message.put("type" , "error");
+			message.put("message" , exceptionMessage);
+			sendData.put("Message" , message);
+			return sendData;
+		}
+	}
 
 
-        }
-        return sendData;
-    }
+
+
+
+	public JSONObject getExerciseStudentByItinerary (  )
+	{
+		//get all the itineraries
+		List<Itinerary> itineraryList = itineraryRepository.findAll();
+		JSONObject sendData = new JSONObject();
+		for (int i = 0; i < itineraryList.size(); i++)
+		{
+			String itineraryId = Integer.toString( itineraryList.get(i).getId() );
+			JSONObject returnJson = getExerciseStudentByItinerary ( itineraryId );
+			sendData.put("Itinerary_" + itineraryId , returnJson );
+
+
+		}
+		return sendData;
+	}
+
+
+
+	public List<UserExercice> getByStudent(MyAppUser student) {	
+
+
+		List<UserExercice> allUserExercices = userExerciceRepository.findAll();
+		List<UserExercice> userExercicesByStudent = new ArrayList<UserExercice>();
+
+
+		for (int i=0; i<allUserExercices.size(); i++) {
+
+			if (allUserExercices.get(i).getUserStudent() != null) {
+
+				if (student.getId().equals(allUserExercices.get(i).getUserStudent().getId())) {
+					userExercicesByStudent.add(allUserExercices.get(i));
+
+				}
+			}
+		}
+
+		return userExercicesByStudent;
+	}
+
+
+
 
 }
+
+
